05abaed047132246985e34e534f50152ded13917
hayoreo <nimjero@gmail.com>
2022-08-07 20:24:24 -0700

    Hide the mouse cursor while inactive.
    If the mouse doesn't move for 5 seconds, hide the cursor.

--- BGAnimations/_modules/Mouse Cursor/default.lua
+++ BGAnimations/_modules/Mouse Cursor/default.lua
@@ -2,7 +2,9 @@ local MouseX
 local MouseY
 local XMax = SCREEN_WIDTH
 local YMax = SCREEN_HEIGHT
-local Refresh = 1/PREFSMAN:GetPreference("RefreshRate")
+local RefreshRate = PREFSMAN:GetPreference("RefreshRate")
+local Refresh = 1/RefreshRate
+local HideMouseCounter = 0
 
 local af = Def.ActorFrame{
 	Def.Sprite{
@@ -24,13 +26,27 @@ local af = Def.ActorFrame{
 		end,
 		UpdateMouseCommand=function(self)
 			self:stoptweening()
+			local PastMouseX = MouseX
+			local PastMouseY = MouseY
 			MouseX = INPUTFILTER:GetMouseX()
 			MouseY = INPUTFILTER:GetMouseY()
+			
+			-- Check if the mouse has moved on this update, if it hasn't increase the counter.
+			if PastMouseX == MouseX and PastMouseY == MouseY then
+				HideMouseCounter = HideMouseCounter + 1
+			else
+				HideMouseCounter = 0
+			end
+			
+			-- If the mouse is out of bounds hide it. If the mouse has been stationary for more than 5 seconds also hide it.
 			if (MouseX < 0 or MouseX > XMax) or (MouseY < 0 or MouseY > YMax) then
+				self:visible(false)
+			elseif HideMouseCounter > RefreshRate * 5 then
 				self:visible(false)
 			else
 				self:visible(true)
 			end
+			
 			self:xy(MouseX,MouseY)
 			self:sleep(Refresh):queuecommand("UpdateMouse")
 		end,
