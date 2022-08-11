c48917749b9fea0312ab410512b54a4b2cd22414
hayoreo <nimjero@gmail.com>
2022-08-11 02:14:07 -0700

    Update mouse cursor module.
    We don't want to run the risk of the mouse counter overflowing if someone leaves their computer idle for a bit so let's cap the counter at where we're hiding the cursor anyway.

--- BGAnimations/_modules/Mouse Cursor/default.lua
+++ BGAnimations/_modules/Mouse Cursor/default.lua
@@ -5,6 +5,7 @@ local YMax = SCREEN_HEIGHT
 local RefreshRate = PREFSMAN:GetPreference("RefreshRate")
 local Refresh = 1/RefreshRate
 local HideMouseCounter = 0
+local MaxMouseCounter = RefreshRate * 3
 
 local af = Def.ActorFrame{
 	Def.Sprite{
@@ -32,16 +33,18 @@ local af = Def.ActorFrame{
 			MouseY = INPUTFILTER:GetMouseY()
 			
 			-- Check if the mouse has moved on this update, if it hasn't increase the counter.
-			if PastMouseX == MouseX and PastMouseY == MouseY then
+			if PastMouseX == MouseX and PastMouseY == MouseY and HideMouseCounter < MaxMouseCounter then
 				HideMouseCounter = HideMouseCounter + 1
+			elseif HideMouseCounter == MaxMouseCounter and PastMouseX == MouseX and PastMouseY == MouseY then
+				-- don't increment if we're already maxed i don't want to deal with potential overflow lol
 			else
 				HideMouseCounter = 0
 			end
 			
-			-- If the mouse is out of bounds hide it. If the mouse has been stationary for more than 5 seconds also hide it.
+			-- If the mouse is out of bounds hide it. If the mouse has been stationary for more than 3 seconds also hide it.
 			if (MouseX < 0 or MouseX > XMax) or (MouseY < 0 or MouseY > YMax) then
 				self:visible(false)
-			elseif HideMouseCounter > RefreshRate * 5 then
+			elseif HideMouseCounter >= MaxMouseCounter then
 				self:visible(false)
 			else
 				self:visible(true)
