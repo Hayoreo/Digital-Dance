3cebc43fe2c76ae0b0e0704cb93798c96adfc010
hayoreo <nimjero@gmail.com>
2023-01-29 18:03:55 -0800

    Readjust letter grade to fit with breakdown text.
    Make the letter grade slightly smaller/higher to accommodate the breakdown text.

--- BGAnimations/ScreenEvaluation common/PerPlayer/Upper/LetterGrade.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/Upper/LetterGrade.lua
@@ -8,9 +8,9 @@ local t = Def.ActorFrame{}
 t[#t+1] = LoadActor(THEME:GetPathG("", "_grades/"..grade..".lua"), playerStats)..{
 	InitCommand=function(self)
 		self:x(70 * (player==PLAYER_1 and -1 or 1))
-		self:y(_screen.cy-134)
+		self:y(_screen.cy-138)
 	end,
-	OnCommand=function(self) self:zoom(0.4) end
+	OnCommand=function(self) self:zoom(0.38) end
 }
 
 return t
